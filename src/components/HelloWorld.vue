2169f5c0d876103a3d8eb0509d48cbd82020012d
daodao <15531083675@163.com>
2020-07-22 17:51:01 +0800

    vue 2020.07.22

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
+    <p>{{ number }}</p>
     <p>{{ time }}</p>
+    <input type="text" v-model="test">
   </div>
 </template>
 
@@ -9,12 +11,28 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      time: '2020-07-16'
+      time: new Date(),
+      test: '',
+      number: 0
     }
   },
   computed: {
     reversed: function(){
       return this.test.split('').reverse().join('');
+    }
+  },
+  created: function(){
+    this.debounceGetAnswer = _.debounce(this.getAnswer, 500)
+  },
+  methods: {
+    getAnswer: function(){
+      this.number++;
+    }
+  },
+  watch: {
+    test: function(){
+      this.time = new Date();
+      this.debounceGetAnswer()
     }
   }
 }
